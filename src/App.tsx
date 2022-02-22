7836d93e12decf7a9b92a899143d287e50491290
sambuite <murilosambuite@gmail.com>
2022-02-22 15:19:44 -0300

    Created NewTransactionModal component with react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { Dashboard } from 'components/Dashboard';
 import { Header } from 'components/Header';
+import { NewTransactionModal } from 'components/NewTransactionModal';
+import { useState } from 'react';
 import { GlobalStyle } from 'styles/global';
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
